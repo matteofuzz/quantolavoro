f425821c9f2371ad539857b6b4ecf190de459be4
Matteo Folin <matteo.folin@gmail.com>
2012-12-04 17:06:50 +0100

    Commit iniziale app rails vuota

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Quantolavoro::Application.config.secret_token = 'd6c72879d7a82684bc890643ab46da0ca98d5060c4cc039e438a3d28f00ce622b1c429e836b395eab80cef80ad694f1e3a9eb6bd91b657688668f55fdb6f84be'
