f29f32a33ed9487f980251ddd52fefd2744fd0e0
Matteo Folin <matteo.folin@gmail.com>
2013-01-15 15:56:43 +0100

    better hm helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,9 +2,13 @@ module ApplicationHelper
   def minutes_as_hm(minutes)
     if minutes.to_i > 0
       hm = minutes.divmod 60
-      "#{hm[0]}:#{hm[1]}"
+      if hm[1] == 0
+        "#{hm[0]}h"
+      else
+        "#{hm[0]}h#{hm[1]}m"
+      end
     else
-      '00:00'
+      '-'
     end
   end
 end
